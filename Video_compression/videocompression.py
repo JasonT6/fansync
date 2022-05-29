c28e1a7a738d2401073ad9e2efcbc8fd1c59a583
JasonT6 <jjtai66@gmail.com>
2022-05-29 03:09:25 -0400

    more organized code

--- Video_compression/videocompression.py
+++ Video_compression/videocompression.py
@@ -3,13 +3,16 @@ import cv2
 import numpy as np
 import json
 
-vid = cv2.VideoCapture("Video_compression/vid1.mp4")
+#   location of cheer video
+vid = cv2.VideoCapture("Video_compression/vid3.mp4")
 
 fps = vid.get(cv2.CAP_PROP_FPS)
-# print("Frame rate: ", int(fps), "FPS")
 
-height = 15
-width = 60
+#   Required height of video based on seat map
+height = 28
+
+#   Required width of video based on seat map
+width = 78
 array = []
 frameCount = 0
 success = True
@@ -18,29 +21,11 @@ while success == True:
     success, frame_prime = vid.read()
     if success == True:
         frame = cv2.resize(frame_prime,(width, height),fx = 0, fy = 0, interpolation = cv2.INTER_AREA)
-        # cv2.imshow("frame", frame)
         array.append(frame)
         frameCount += 1
 
 tempArray = np.array(array).tolist()
-# jsonArray = json.dumps(tempArray)
 
-with open('Video_compression/jsonArray.json', 'w') as f:
+#   Output array of video at required resolution
+with open('Video_compression/jsonArray3.json', 'w') as f:
     json.dump(tempArray,f)
-
-
-    # if cv2.waitKey(1) == ord('q'):
-    #     break
-# print(frameCount)
-# print("This is pixel[50,50] frame 149")
-# print(array[149][50][50])
-# condensed = []
-
-# for count, value in enumerate(array):
-#     if(count - 1 < 0):
-#         condensed.append([array[count][7][30], count])
-#         continue
-#     elif (array[count-1][7][30][0] != array[count][7][30][0]):
-#         condensed.append([array[count][7][30], count])
-
-# print(condensed)
